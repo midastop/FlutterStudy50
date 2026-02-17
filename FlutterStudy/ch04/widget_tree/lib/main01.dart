c89b2b6c1fe7483d12e63937130b044771848a9e
midastop <midastop@naver.com>
2026-02-18 03:00:12 +0900

    FlutterStudy/ch04/widget_tree 완료

---
+++ FlutterStudy/ch04/widget_tree/lib/main01.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // 플러터 앱을 실행해 주는 함수
+  runApp(const MyApp());
+}
+
+// StatelessWidget으로 위젯 트리 구성
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // MaterialApp은 Material 디자인을 사용할 수 있도록 지원하는 위젯으로
+    // Flutter 앱에서 항상 최상위에 MaterialApp 위젯을 사용해야 한다.
+    return MaterialApp(  // Root Widget
+      // 화면 우측 상단에 표시되는 DEBUG 베너 제거 설정
+      debugShowCheckedModeBanner: false,
+
+      // MaterialApp 바로 아래 home 파라미터에 Scaffold 위젯을 사용
+      // Scaffold 위젯은 화면 하나의 전체 구조를 만들 수 있도록 지원하는 위젯
+      home: Scaffold(
+        // AppBar는 앱 상단에 표시되는 위젯으로 메뉴, 제목 등을 표시하거나 이외에 다양한 용도로 활용할 수 있다.
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Flutter Widget Tree"),
+        ),
+
+        // Scaffold의 body 파라미터에 다양한 위젯을 배치해 화면을 구성한다.
+        // 위젯은 child 또는 children 파라미터를 사용해 하위에 자식 위젯을 배치한다.
+        // child는 하나의 위젯을 지정할 수 있고 children은 여러 위젯을 지정할 수 있다.
+        // child와 children 파라미터 두 가지를 모두 제공하지는 않으며 위젯에 따라서
+        // child 파라미터를 제공하거나 children 파라미터를 제공한다.
+        // Center는 자식 위젯을 부모 위젯에 정 가운데 표시하는 위젯임
+        body: Center(
+          // Column 위젯은 여러 개의 자식 위젯을 세로 방향으로 배치할 수 있는 위젯임
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Text Widget",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Color(0xFF963AC1),
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  print("Elevated 버튼 클릭됨");
+                },
+                child: Text("ElevatedButton"),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("플로팅 버튼 클릭됨");
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
